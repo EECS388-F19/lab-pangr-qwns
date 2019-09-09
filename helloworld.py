b69b325af3a5864d3c979871788d9135cd1917a7
pangr <pangr@umich.edu>
2019-09-09 12:30:57 -0400

    lab0 python tutorial

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print('Raymond Pang')
+import random
+r1 = random.randint(1,100)
+r2 = random.randint(1,100)
+print(r1)
+print(r2)
+print('Sum = {}'.format(r1+r2))
+print('Average = {}'.format((r1+r2)/2))
